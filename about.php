5ab44d30440ab0a6ff61687f91e8ff7dd51b7adb
Denilson <denuariua@gmail.com>
2022-09-04 15:37:41 +0100

    added about us page

---
+++ about.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Bootstrap CSS-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title> NamEduHire</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sriracha&display=swap" rel="stylesheet">
+    <!--Link HTML and CSS file-->
+    <link rel="stylesheet" href="./style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <!-- About us page with a navbar and body container containing text -->
+    <!-- start of navbar -->
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="index.html"><img src="./images/eduhirelogo.png" width="45" height="43"
+                    alt="logo"> NamEduHire</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav m-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="findJob.php">Find a Job</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="postJop.php">Post a Job</a>
+                    </li>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- end of navbar -->
+    <!-- start of body containing title and paragraph text  -->
+    <div class="p-5"></div>
+    <div class="container bg-light rounded">
+        <!-- Content here -->
+        <h1 class="text-center">About NamEduHire</h1>
+        <div class="p-3 ">
+        <p class="h4">One of them most urgent development challenges in Namibia over the past three decades
+            is unemployment amongst qualified teachers. Hanghuwo (2019) reported that it is evident
+            that more qualified teachers exist over the number of available positions to fill.
+            To which the current Namibian Minister of Education, Arts and Culture; Hon. Anna Nghipondoka
+            responded that the Ministry will only provide a total of 1600 teaching vacancies as of 2019,
+            which merely amounts to a little over 60% of the number of qualified teachers that were produced in
+            that year. “Amongst the total number of 2019 graduates, over 2000 students came from the faculty of
+            education.” (Iikela.S,2020) The majority of which are still unemployed or between jobs.
+            Tubaundula (2021) believes that the main issue is the lack of available jobs for the annual
+            high influx of teachers. There are many instances where, when teachers finally see the vacancy, it is
+            way past the due date. With this in mind it is safe to believe that it is vital for every teacher to have
+            equal opportunities at accessing available vacancies on time at a centralized platform.</p>
+    </div>
+    </div>
+
+</body>
+</html>
--- login.php
+++ login.php
@@ -38,7 +38,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav m-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link" href="#">About</a>
+              <a class="nav-link" href="about.php">About</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="findJob.php">Find a Job</a>
