95dd46a24d6d3d51e78d9012fd1d196fe842d8c5
Denilson <denuariua@gmail.com>
2022-09-18 21:12:57 +0200

    dbConfig comments

--- dbConfig.php
+++ dbConfig.php
@@ -1,6 +1,14 @@
+
+<!-- The dbConfig.php file allows us to create a single file that connects to the MYSQL 
+database once and import it where ever it is needed thoughout he project -->
+
 <?php
 //connect to database
 //create variables for the database connection
+// $host is the servername to connect to
+// $user is the username to connect to the database
+// $password is the password to connect to the database
+// $database is the name of the database
 $host = 'localhost';
 $database = 'test';
 $user = 'root';
