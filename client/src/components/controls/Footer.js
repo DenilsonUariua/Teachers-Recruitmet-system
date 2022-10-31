77e75c2b511f6545e2f55aa6ea9850a673309267
Denilson <denuariua@gmail.com>
2022-10-31 14:51:20 +0200

    grommet starting file

---
+++ client/src/components/controls/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import {
+    Box,
+  } from "grommet";
+
+const Footer = ({ ...rest }) => (
+  <Box
+    as="footer"
+    align="center"
+    direction="row"
+    flex={false}
+    gap="medium"
+    justify="between"
+    {...rest}
+  />
+);
+
+export { Footer };
---
+++ client/src/components/controls/Header.js
@@ -0,0 +1,63 @@
+import React from "react";
+import {
+  Box,
+  Button,
+  Collapsible,
+  Heading,
+  Grommet,
+  Layer,
+  ResponsiveContext
+} from "grommet";
+import { FormClose, Notification } from "grommet-icons";
+
+const theme = {
+  global: {
+    colors: {
+      brand: "#228BE6"
+    },
+    font: {
+      family: "Roboto",
+      size: "14px",
+      height: "20px"
+    }
+  }
+};
+
+const AppBar = props => (
+  <Box
+    tag="header"
+    direction="row"
+    align="center"
+    justify="between"
+    background="brand"
+    pad={{ left: "medium", right: "small", vertical: "small" }}
+    elevation="medium"
+    style={{ zIndex: "1" }}
+    {...props}
+  />
+);
+
+export const Header = () => {
+  const [showSidebar, setShowSidebar] = React.useState(false);
+
+  return (
+    <Grommet theme={theme} full>
+      <ResponsiveContext.Consumer>
+        {size => (
+          <Box fill>
+            <AppBar>
+              <Heading level="3" margin="none">
+                My App
+              </Heading>
+              <Button
+                icon={<Notification />}
+                onClick={() => setShowSidebar(!showSidebar)}
+              />
+            </AppBar>
+          </Box>
+        )}
+      </ResponsiveContext.Consumer>
+    </Grommet>
+  );
+};
+
