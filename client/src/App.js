2b09af70046a7da53e3136475be4447ece83bc74
Celine <namutenya.ch@gmail.com>
2022-11-02 10:37:02 +0200

    pocketbase

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,26 @@
 import React from "react";
 import { Header, Footer } from "@controls";
 import { Text, Anchor, Grid, Box } from "grommet";
+import PocketBase from 'pocketbase';
+
+const client = new PocketBase('http://127.0.0.1:8090');
+
+async function fetchValues(){
+
+// fetch a paginated records list
+const resultList = await client.records.getList('jobs', 1, 50, {
+    filter: 'created >= "2022-01-01 00:00:00"',
+});
+
+// alternatively you can also fetch all records at once via getFullList:
+const records = await client.records.getFullList('jobs', 200 /* batch size */, {
+    sort: '-created',
+});
+console.log('records', records);
+}
 
 const App = () => {
+ fetchValues();
   return (
     <div>
       <Grid
