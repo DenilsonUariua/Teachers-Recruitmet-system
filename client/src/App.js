9e2c533c226935db19d2a6d14cba8e11092ee86f
Celine <namutenya.ch@gmail.com>
2022-11-02 11:33:10 +0200

    new start

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,38 @@
 import React from "react";
 import { Header, Footer } from "@controls";
 import { Text, Anchor, Grid, Box } from "grommet";
-import PocketBase from 'pocketbase';
+import PocketBase from "pocketbase";
 
-const client = new PocketBase('http://127.0.0.1:8090');
+const client = new PocketBase("http://127.0.0.1:8090");
 
-async function fetchValues(){
-
-// fetch a paginated records list
-const resultList = await client.records.getList('jobs', 1, 50, {
-    filter: 'created >= "2022-01-01 00:00:00"',
-});
-
+async function fetchValues() {
+  
+  // admin authentication via email/pass
+  const adminAuthData = await client.admins.authViaEmail('namutenya.ch@gmail.com', 'celinehaiketi');
+  
 // alternatively you can also fetch all records at once via getFullList:
 const records = await client.records.getFullList('jobs', 200 /* batch size */, {
-    sort: '-created',
+  sort: '-created',
 });
-console.log('records', records);
+  console.log("records", records);
+  localStorage.setItem("token", adminAuthData.token);
 }
 
 const App = () => {
- fetchValues();
+  fetchValues();
   return (
     <div>
+      <Header />
       <Grid
-        rows={["xxsmall", "xsmall"]}
-        columns={["xsmall", "small"]}
+        rows={["auto", "flex"]}
+        columns={["auto", "flex"]}
         gap="small"
-        areas={[
-          { name: "header", start: [0, 0], end: [1, 0] },
-          { name: "nav", start: [0, 1], end: [0, 1] },
-          { name: "main", start: [1, 1], end: [1, 1] }
-        ]}
       >
         <Box gridArea="header" background="brand" />
-        <Box gridArea="nav" background="light-5" />
         <Box gridArea="main" background="light-2" />
+
+        <Box gridArea="sidebar" background="light-4" />
+        <Box gridArea="footer" background="brand" />
       </Grid>
     </div>
   );
